456152eebff01cdf7b87c801f86e73e78a3b8831
akalManNakarmi <akalnakarmi1@gmail.com>
2023-01-06 16:26:22 +0545

    day 2

---
+++ day_2/variables.py
@@ -0,0 +1,53 @@
+# Day 2: 30 Days of python programming
+
+firstName = "fName"
+lastName = "lName"
+fullName = "fullName"
+country = "country"
+city = "city"
+age = 10
+year = 2023
+is_married = False
+is_true = True
+is_light_on = True
+
+a,b,c,d = 1,2,3,4
+
+type(firstName)
+type(lastName)
+type(fullName)
+type(country)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+type(is_light_on)
+type(a)
+type(b)
+type(c)
+type(d)
+
+len(firstName)
+
+len(firstName)>len(lastName)
+
+num_one , num_two = 5,4
+total = num_one+num_two
+diff = num_one-num_two
+product = num_one*num_two
+division = num_one/num_two
+remainder = num_one%num_two
+exp = num_one**num_two
+floor_division = num_one//num_two
+
+area_of_circle = 3.14*(30**2)
+circum_of_circle = 2*3.14*30
+radious = input("Enter Radious: ")
+area = 3.14*(radious**2)
+
+fName = input("Enter First Name: ")
+lName = input("Enter Last Name: ")
+country = input("Enter Country: ")
+age = input("Enter Age: ")
+
+help('keywords')
