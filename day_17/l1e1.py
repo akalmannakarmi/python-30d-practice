51a1d855940b5790ca7bc74dc2c07c1f28c3f996
akalManNakarmi <akalnakarmi1@gmail.com>
2023-02-02 19:42:14 +0545

    unpacking correction

--- day_17/l1e1.py
+++ day_17/l1e1.py
@@ -1,11 +1,7 @@
 # names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']. Unpack the first five countries and store them in a variable nordic_countries, store Estonia and Russia in es, and ru respectively.
 
 names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
-
-nordic_countries = [names.pop(0) for i in range(5)]
-es = names.pop(0)
-ru = names.pop(0)
-
+*nordic_countries,es,ru = names
 print(nordic_countries)
 print(es)
 print(ru)
